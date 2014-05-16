3276d6a5045ef2198e0eaa0f41abb9abec97a701
filipelamberti <filipelamberti@gmail.com>
2014-05-16 11:48:53 -0300

    Alterando

--- fluxo_caixa/views.py
+++ fluxo_caixa/views.py
@@ -13,8 +13,12 @@ def fluxoPesquisar(request):
         data_Inicial = request.POST.get('data_Inicial', '%d/%m/%Y')
         data_Final = request.POST.get('data_Final', '%d/%m/%Y')
         total = 0
+
+       
+            contas = Conta.objects.filter(data__range=(data_Inicial, data_Final))
+            for conta in contas:
+                total += conta.valor
         
-        except:
             contas = []
 
         return render(request, 'fluxo_caixa/lista_fluxo.html', {'contas': contas, 'total': total, 'data_Inicial': data_Inicial, 'data_Final': data_Final})
